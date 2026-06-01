b753008bdd332a0dbc7db465c6787551b09a54f6
luscasmbs <lucasmiguelbsilva@gmail.com>
2026-06-01 16:25:51 -0300

    feat: header da tela de pesquisa

---
+++ app/src/main/java/br/senai/catalogoaprender/ui/components/SearchHeader.kt
@@ -0,0 +1,86 @@
+package br.senai.catalogoaprender.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.catalogoaprender.R.drawable.logo
+
+@Composable
+fun SearchHeader(modifier: Modifier = Modifier){
+    Column(modifier = modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+                .background(Color(0xFF0d1b2a))
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .padding(top = 37.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(logo),
+                    contentDescription = "Logo do app",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                )
+
+                Text(
+                    text = "APRENDER+",
+                    modifier = Modifier.padding(start = 12.dp),
+                    style = MaterialTheme.typography.labelSmall,
+                    color = Color(0xFF5cacd0),
+                    fontSize = 13.sp
+                )
+            }
+
+            Text(
+                text = "Catálogo Aprender+",
+                modifier = Modifier
+                    .padding(top = 98.dp)
+                    .padding(start = 12.dp),
+                color = Color(0xFFF4F5F0),
+                style = MaterialTheme.typography.labelSmall,
+                fontSize = 23.sp
+            )
+
+            Text(
+                text = "Explore cursos técnicos e profissionalizantes.",
+                modifier = Modifier
+                    .padding(top = 133.dp)
+                    .padding(start = 12.dp),
+                color = Color(0xFF5cacd0),
+                style = MaterialTheme.typography.labelSmall,
+                fontSize = 13.sp
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun SearchHeaderPreview(){
+    SearchHeader()
+}
